76b5adecaeea130b83c023e766e0030a3a428224
Davoud Dourvash <DDourvash@GC.Local>
2023-07-02 12:20:28 +0330

    day 1 answers added.

---
+++ day1_Exc.py
@@ -0,0 +1,49 @@
+# day 1 Exercises 
+# Ex1:
+# 1
+### python 3.11.3
+# 2
+print(3+4)
+print(3-4)
+print(3*4)
+print(3%4)
+print(3/4)
+print(3**4)
+print(3//4)
+
+
+# 3
+print('My Name is ', 'David')
+print('My Family Name is ', 'Dourvash')
+print('My Country is ',  'Iran')
+print('I am enjoying 30 days of python')
+
+#4
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['David', 'Python', 'File']))
+print(type('David'))
+print(type('Dourvash'))
+print(type('Iran'))
+
+
+# EX2
+## Done
+print(type(3)) #int
+print(type(3.2)) #float
+print(type(4+3j)) #complex
+print(type('David')) #string
+print(type(True)) #boolean
+print(type(['3.3', 12, 3.2]))
+print(type({'name' : 'David', 'Age' : 33, 'Father Name' : 'Farajallah'}))
+print(type({3.3, 25, 12}))
+print(type((3.8, 3.2, 44)))
+
+
+
+# EX3
+import math
+print(math.sqrt((2-3)**2))
+print(math.sqrt((10-8)**2))
